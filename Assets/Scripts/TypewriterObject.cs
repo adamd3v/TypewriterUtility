22e0b860276f6ceb4d5e75c9f831810c921a7fb1
adamdev <hmullinsk@hotmail.com>
2021-07-07 04:19:03 -0400

    Initial commit

---
+++ Assets/Scripts/TypewriterObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Typewriter Object", menuName = "Typewriter Object")]
+public class TypewriterObject : ScriptableObject
+{
+    public enum SpeakerType
+    {
+        Speaker_Default,
+        Speaker_Deep,
+        Speaker_High
+    }
+
+    public SpeakerType type;
+    public float pitch;
+
+    [TextArea(0, 500)]
+    public string message;
+}
